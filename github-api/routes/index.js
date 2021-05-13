8aa015b3ed95596c56741eaffd70bb05c2781a46
mm-masahiro <hand5.mm9.mzh@gmail.com>
2021-05-14 01:37:10 +0900

    fetch github api

--- github-api/routes/index.js
+++ github-api/routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
+const axios = require('axios');
+const { response } = require('express');
+const app = express();
 var router = express.Router();
+
+app.use(express.static('views'));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hello Express!!' });
+  const person = [
+    { name: 'masahiro', age: 24 },
+    { name: 'shonosule', age: 24 }
+  ];
+  res.json(person);
 });
+
+router.get('/github-issue', function(req, res, next) {
+  axios.get('https://api.github.com/repos/mm-masahiro/profile-portfolio/issues/135').then(
+    function(response) {
+      // console.log(response.data)
+      res.send(response.data)
+    }
+  )
+})
 
 module.exports = router;
