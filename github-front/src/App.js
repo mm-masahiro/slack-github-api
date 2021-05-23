eea7ffe06b55f2a6cb29797ed04b87f133854179
mm-masahiro <hand5.mm9.mzh@gmail.com>
2021-05-24 00:02:52 +0900

    create front env

---
+++ github-front/src/App.js
@@ -0,0 +1,22 @@
+// import React, { useState, useEffect } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  // const [issues, setIssues] = useState([]);
+
+  // React.useEffect(
+  //   fetch('/github-issue').then(
+  //     res => res.json()
+  //   ).then(
+  //     issues => setIssues(issues)
+  //   )
+  // )
+  return (
+    <div className="App">
+      <div>issues</div>
+    </div>
+  );
+}
+
+export default App;
